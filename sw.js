385b623839042e646da397cad0fe46f1c273c424
Bruno Eduardo <brunoeduardocoe@gmail.com>
2019-06-08 19:31:03 -0300

    Changes on sw links to work on gh-pages

--- sw.js
+++ sw.js
@@ -1,33 +1,32 @@
 //Update cache names any time any of the cached files change.
 const CACHE_NAME = 'static-cache-v1';
 const DATA_CACHE_NAME = 'data-cache-v1';
-//for github pages
-const repository = '/facial-recognition-pwa/';
+
 // Add list of files to cache here.
 const FILES_TO_CACHE = [
-  '/facial-recognition-pwa/',
-  repository+'/index.html',
-  repository+'css/bootstrap.min.css',
-  repository+'css/material.min.css',
-  repository+'css/style.css',
-  repository+'js/bootstrap.min.js' ,
-  repository+'js/face-api.js',
-  repository+'js/jquery-3.4.1.slim.min.js' ,
-  repository+'js/main.js',
-  repository+'js/models/face_expression_model-shard1',
-  repository+'js/models/face_expression_model-weights_manifest.json',
-  repository+'js/models/face_landmark_68_model-shard1',
-  repository+'js/models/face_landmark_68_model-weights_manifest.json',
-  repository+'js/models/face_landmark_68_tiny_model-shard1',
-  repository+'js/models/face_landmark_68_tiny_model-weights_manifest.json',
-  repository+'js/models/face_recognition_model-shard1',
-  repository+'js/models/face_recognition_model-shard2',
-  repository+'js/models/face_recognition_model-weights_manifest.json',
-  repository+'js/models/tiny_face_detector_model-shard1',
-  repository+'js/models/tiny_face_detector_model-weights_manifest.json',
-  repository+'imgs/undraw_profile_pic_ic5t.svg',
-  repository+'imgs/icons/icons.png',
-  repository+'manifest.json'
+  '/',
+  '/index.html',
+  'css/bootstrap.min.css',
+  'css/material.min.css',
+  'css/style.css',
+  'js/bootstrap.min.js' ,
+  'js/face-api.js',
+  'js/jquery-3.4.1.slim.min.js' ,
+  'js/main.js',
+  'js/models/face_expression_model-shard1',
+  'js/models/face_expression_model-weights_manifest.json',
+  'js/models/face_landmark_68_model-shard1',
+  'js/models/face_landmark_68_model-weights_manifest.json',
+  'js/models/face_landmark_68_tiny_model-shard1',
+  'js/models/face_landmark_68_tiny_model-weights_manifest.json',
+  'js/models/face_recognition_model-shard1',
+  'js/models/face_recognition_model-shard2',
+  'js/models/face_recognition_model-weights_manifest.json',
+  'js/models/tiny_face_detector_model-shard1',
+  'js/models/tiny_face_detector_model-weights_manifest.json',
+  'imgs/undraw_profile_pic_ic5t.svg',
+  'imgs/icons/icons.png',
+  'manifest.json'
 
 ];
 
