dbc7ed4f22d22ca30391b33cf058ad36e28c740d
Bruno Eduardo <brunoeduardocoe@gmail.com>
2019-06-08 20:22:46 -0300

    Not used models commented to improve performance on mobile

--- js/main.js
+++ js/main.js
@@ -19,9 +19,9 @@
     	//Load models for face recognition net
 		Promise.all([
 		  faceapi.nets.tinyFaceDetector.loadFromUri('js/models/'),
-		  faceapi.nets.faceLandmark68Net.loadFromUri('js/models/'),
-		  faceapi.nets.faceRecognitionNet.loadFromUri('js/models/'),
-		  faceapi.nets.faceExpressionNet.loadFromUri('js/models/')
+		  // faceapi.nets.faceLandmark68Net.loadFromUri('js/models/'),
+		  // faceapi.nets.faceRecognitionNet.loadFromUri('js/models/'),
+		  // faceapi.nets.faceExpressionNet.loadFromUri('js/models/')
 		]).then(startVideo)
 
 		video.addEventListener('play', () => {
@@ -31,13 +31,13 @@
 		  const displaySize = { width: video.width, height: video.height }
 		  faceapi.matchDimensions(canvas, displaySize)
 		  setInterval(async () => {
-		    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+		    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())/*.withFaceLandmarks().withFaceExpressions()*/
 		    const resizedDetections = faceapi.resizeResults(detections, displaySize)
 		    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
 		    faceapi.draw.drawDetections(canvas, resizedDetections)
 		    // faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
 		    // faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-		  }, 3000)
+		  }, 1500)
 		})
 		
     });
