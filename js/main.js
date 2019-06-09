094c91b1ab458317d30f91545d9c07b849058473
Bruno Eduardo <brunoeduardocoe@gmail.com>
2019-06-08 21:09:44 -0300

    Smaller input size

--- js/main.js
+++ js/main.js
@@ -31,7 +31,7 @@
 		  const displaySize = { width: video.width, height: video.height }
 		  faceapi.matchDimensions(canvas, displaySize)
 		  setInterval(async () => {
-		    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())/*.withFaceLandmarks().withFaceExpressions()*/
+		    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions({ inputSize: 128 }))/*.withFaceLandmarks().withFaceExpressions()*/
 		    const resizedDetections = faceapi.resizeResults(detections, displaySize)
 		    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
 		    faceapi.draw.drawDetections(canvas, resizedDetections)
